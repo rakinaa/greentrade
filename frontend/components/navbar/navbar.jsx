2e11591eea9d4a5a58e6b8c8757ba459a01ba0ef
rakin <rakinadm19@gmail.com>
2020-06-06 06:06:22 -0700

    Changed logo

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -8,7 +8,7 @@ const NavBar = (props) => {
     <nav className="nav-main">
       <div className="constrainer">
         <div className="nav-left">
-          <Link className="nav-logo link-white" to="/">Pictr</Link>
+          <Link className="nav-logo link-white" to="/">Greentrade</Link>
           <Link className="nav-home link-white" to="/photos">All Photos</Link>
           {props.currentUser ? <Link className="nav-home link-white" to={`/users/${props.currentUser.id}/photos`}>Home</Link> : null}
         </div>
