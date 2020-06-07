c03fd122b04dbfd62bdf686ec17a3e3617421449
rakin <rakinadm19@gmail.com>
2020-06-06 19:52:46 -0700

    Increased darkening of nav button

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -11,10 +11,8 @@ const NavBar = (props) => {
         <img className="nav-img" src={window.logo_png} alt="logo"/>
         <Link className="nav-home link-grey" to="/">Learn</Link>
         <Link className="nav-home link-grey" to="/">Support</Link>
-        {/* {props.currentUser ? <Link className="nav-home link-white" to={`/users/${props.currentUser.id}/photos`}>Home</Link> : null} */}
       </div>
       <div className="nav-right">
-        {/* <SearhBar /> */}
         <GreetingContainer />
       </div>
     </nav>
