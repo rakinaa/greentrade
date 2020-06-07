10e4fd3de7de7f661bdd006408a051a00c5877ce
rakin <rakinadm19@gmail.com>
2020-06-06 19:35:52 -0700

    Fixed padding of navbar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -6,18 +6,16 @@ import SearhBar from './searchbar';
 const NavBar = (props) => {
   return (
     <nav className="nav-main">
-      <div className="constrainer">
-        <div className="nav-left">
-          <p className="nav-logo" to="/">Greentrade</p>
-          <img className="nav-img" src={window.logo_png} alt="logo"/>
-          <Link className="nav-home link-grey" to="/">Learn</Link>
-          <Link className="nav-home link-grey" to="/">Support</Link>
-          {/* {props.currentUser ? <Link className="nav-home link-white" to={`/users/${props.currentUser.id}/photos`}>Home</Link> : null} */}
-        </div>
-        <div className="nav-right">
-          {/* <SearhBar /> */}
-          <GreetingContainer />
-        </div>
+      <div className="nav-left">
+        <p className="nav-logo" to="/">Greentrade</p>
+        <img className="nav-img" src={window.logo_png} alt="logo"/>
+        <Link className="nav-home link-grey" to="/">Learn</Link>
+        <Link className="nav-home link-grey" to="/">Support</Link>
+        {/* {props.currentUser ? <Link className="nav-home link-white" to={`/users/${props.currentUser.id}/photos`}>Home</Link> : null} */}
+      </div>
+      <div className="nav-right">
+        {/* <SearhBar /> */}
+        <GreetingContainer />
       </div>
     </nav>
   )
