d9de7551b819769649c38b011edc3eb2fdce90a4
rakin <rakinadm19@gmail.com>
2020-06-08 06:12:20 -0700

    Added status scroll bar

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Jumbotron from './jumbotron';
 import NewsContent from './news_content';
 import Carousel from './carousel';
+import Sponsors from './sponsors';
 
 const Splash = (props) => {
   return (
@@ -9,6 +10,7 @@ const Splash = (props) => {
       <Jumbotron />
       <NewsContent />
       <Carousel />
+      <Sponsors />
     </div>
   )
 }
---
+++ frontend/components/splash/sponsors.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sponsors = (props) => {
+  return (
+    <div className="sponsors"></div>
+  )
+}
+
+export default Sponsors;
