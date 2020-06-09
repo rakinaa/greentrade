d14c1af1f240d3f3b8809e6f2bbd52e9f67818a4
rakin <rakinadm19@gmail.com>
2020-06-08 22:19:04 -0700

    Added update functions to login page

---
+++ frontend/components/session_form/login_page.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const LoginPage = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default LoginPage;
