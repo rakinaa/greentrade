7d4f66e3125e48cec99ca2068ec3f200a33595e9
rakin <rakinadm19@gmail.com>
2020-06-17 07:23:28 -0700

    Fixed styling on login page

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -3,7 +3,6 @@ import {
   RECEIVE_CURRENT_USER,
   CLEAR_SESSION_ERRORS,
 } from "../actions/session_actions";
-import { OPEN_MODAL, CLOSE_MODAL } from "../actions/modal_actions";
 
 const sessionErrorsReducer = (state = [], action) => {
   Object.freeze(state);
