db722e1f7bd06191361609fd30bef6a6b676c4e9
rakin <rakinadm19@gmail.com>
2020-10-02 06:15:45 -0700

    Removed references to modal data

--- frontend/components/navbar/greeting/greeting_container.js
+++ frontend/components/navbar/greeting/greeting_container.js
@@ -2,15 +2,13 @@ import { connect } from "react-redux";
 
 import Greeting from "./greeting";
 import { logout } from "../../../actions/session_actions";
-import { openModal } from "../../../actions/modal_actions";
 
 const mapStateToProps = ({ session }) => ({
   currentUser: session.currentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  logout: () => dispatch(logout()),
-  openModal: (modal) => dispatch(openModal(modal)),
+  logout: () => dispatch(logout())
 });
 
 const GreetingContainer = connect(
--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,6 +1,4 @@
 import { combineReducers } from 'redux';
-import modalReducer from './modal_reducer';
-import modalDataReducer from './modal_data_reducer';
 import loadingReducer from './loading_reducer';
 
 const uiReducer = combineReducers({
